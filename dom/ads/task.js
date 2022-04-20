2972e2447307ccb57de2224dc079e9e89b426b8e
colins1 <88490207+colins1@users.noreply.github.com>
2022-04-20 21:37:10 +0300

    add

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,37 +1,20 @@
-/*<div class="card">
-            <p>
-                Я
-                <span class="rotator">
-                <span class="rotator__case rotator__case_active" data-speed="1000" data-color="red">Бог JS</span>
-                <span class="rotator__case" data-speed="2000" data-color="green">Лучший программист на земле</span>
-                <span class="rotator__case" data-speed="1000" data-color="#000">покорю этот мир</span>
-                <span class="rotator__case" data-speed="1000" data-color="red">учусь в Нетологии</span>
-                <span class="rotator__case" data-speed="500" data-color="blue">счастливый как никто</span>
-                <span class="rotator__case" data-speed="200" data-color="gray">радуюсь жизни</span>
-              </span>
-            </p>
-        </div>*/
-
-
-
-    const text = document.querySelectorAll('.rotator__case');
-    let i = 0;
-      setInterval(allText = () => {
-        if (i != text.length - 1) {
-        if (i != 0) {
-          text[i].className = 'rotator__case rotator__case_active';
-          text[i - 1].className = 'rotator__case';
-          i++;
-        } else {
-          text[text.length - 1].className = 'rotator__case';
-          text[i].className = 'rotator__case rotator__case_active';
-          i = i + 1;
-        }
-      } else {
-        text[i - 1].className = 'rotator__case';
-        text[i].className = 'rotator__case rotator__case_active';
-        i = 0;
-      }
-    }, text[i].getAttribute('data-speed'));
-
-      allText();
+const text = document.querySelectorAll('.rotator__case');
+let i = 0;
+  setInterval(allText = () => {
+    if (i != text.length - 1) {
+    if (i != 0) {
+      text[i].className = 'rotator__case rotator__case_active';
+      text[i - 1].className = 'rotator__case';
+      i++;
+    } else {
+      text[text.length - 1].className = 'rotator__case';
+      text[i].className = 'rotator__case rotator__case_active';
+      i = i + 1;
+    }
+  } else {
+    text[i - 1].className = 'rotator__case';
+    text[i].className = 'rotator__case rotator__case_active';
+    i = 0;
+  }
+  text[i].style.color = text[i].getAttribute('data-color')
+}, text[i].getAttribute('data-speed'));
