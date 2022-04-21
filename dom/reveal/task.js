fccdaac7f30da195f0b656c9ff1ecd6bbe7ff852
colins1 <88490207+colins1@users.noreply.github.com>
2022-04-21 07:02:26 +0300

    add

---
+++ dom/reveal/task.js
@@ -0,0 +1,7 @@
+// обрабатываем скролл на всей странице
+document.addEventListener('scroll', function(event) {
+  // реагируем на событие
+
+  let elem = document.elementFromPoint(x, y);
+  console.log(event);
+});
