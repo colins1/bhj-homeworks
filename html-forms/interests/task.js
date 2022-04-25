babb4f321176a10c7d56f4b1f0415df73de1990a
colins1 <88490207+colins1@users.noreply.github.com>
2022-04-25 12:43:14 +0300

    add

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,39 +1,39 @@
-function listActiv1 (listActiv2) {
-  let a = listActiv2.parentElement.parentElement.parentElement;
-  let b = listActiv2.parentElement.parentElement;
-  if (a.className == '') {
-    if (listActiv2.checked == true) {
-      const listActiv3 = b.querySelectorAll('.interest__check');
-      listActiv3.forEach(function(itemList2) {
-        itemList2.checked = true;
+function checkedInterestCheck (input) {
+  let ul = input.closest('ul');
+  let li = input.closest('li');
+  if (ul.className == '') {
+    if (input.checked == true) {
+      const parentChekbox = li.querySelectorAll('.interest__check');
+      parentChekbox.forEach(function(chekbox) {
+        chekbox.checked = true;
       });
     } else {
-      const listActiv3 = b.querySelectorAll('.interest__check');
-      listActiv3.forEach(function(itemList2) {
-        itemList2.checked = false;
+      const parentChekbox = li.querySelectorAll('.interest__check');
+      parentChekbox.forEach(function(chekbox) {
+        chekbox.checked = false;
       });
     }
   } else {
-    const listActiv3 = a.querySelectorAll('.interest__check');
-    if (listActiv2.checked == true) {
-      let q = a.parentElement;
-      q.querySelector('.interest__check').checked = true;
+    const parentChekbox = ul.querySelectorAll('.interest__check');
+    if (input.checked == true) {
+      let parentUl = ul.parentElement;
+      parentUl.querySelector('.interest__check').checked = true;
     } else {
-      let t = Array.from(listActiv3).some(elem => elem.checked == true);
-      if (t) {
-        let q = a.parentElement;
-        q.querySelector('.interest__check').checked = true;
+      let tryeChecked = Array.from(parentChekbox).some(elem => elem.checked == true);
+      if (tryeChecked) {
+        let parentUl = ul.parentElement;
+        parentUl.querySelector('.interest__check').checked = true;
       } else {
-        let q = a.parentElement;
-        q.querySelector('.interest__check').checked = false;
+        let parentUl = ul.parentElement;
+        parentUl.querySelector('.interest__check').checked = false;
       }
     }
   }
 }
 
-const listActiv = document.querySelectorAll('.interest__check');
-listActiv.forEach(function(itemList, i) {
-itemList.addEventListener('click', function(click) {
-  console.log(listActiv1(itemList));
+const allInterestCheck = document.querySelectorAll('.interest__check');
+allInterestCheck.forEach(function(interestCheck) {
+interestCheck.addEventListener('click', function(click) {
+  checkedInterestCheck(interestCheck);
 }, false);
 });
