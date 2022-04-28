a54ec883719ccb1071add029ad0be08c28c6b4fd
colins1 <88490207+colins1@users.noreply.github.com>
2022-04-28 13:51:22 +0300

    add

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,58 +1,39 @@
-/*document.addEventListener('keydown', (event) => {
-            if (this.currentSymbol.textContent == event.key) {
-	            this.success();
-            } else {
-	            this.fail();
-            }
-        });*/
-
-function eva(ev) {
-  ev.remove();
-}
-document.addEventListener('click', (event1) => {
-  if (event1.srcElement.className == 'task__remove') {
-    let n = event1.path[1]
-    n.remove()
-  }
-  console.log(event1)
+document.addEventListener('click', (eventClick) => {
+  eventClick.preventDefault();
+    if (eventClick.srcElement.className == 'task__remove') {
+      eventClick.path[1].remove()
+    }
 }, false);
 
-
-
-const tooltipAll = document.querySelector('.tasks__input');
-tooltipAll.addEventListener('input', function() {
-  const tooltipAll11 = document.querySelector('button');
-  tooltipAll11.addEventListener('click', (event1) => {
-            if (this.value != '') {
-              console.log(this.value);
-              let textHTML = `<div class="task">
-                                 <div class="task__title">
-                                   ${this.value}
-                                 </div>
-                                 <a href="#" class="task__remove">&times;</a>
-                             </div>`;
-               const tooltipAll1 = document.querySelector('.tasks__list');
-               tooltipAll1.insertAdjacentHTML('afterBegin', textHTML);
-               this.value = '';
-            }
-  });
-  document.addEventListener('keydown', (event) => {
-
-
-
-  if (event.code == 'Enter') {
+const tasksInput = document.querySelector('.tasks__input');
+tasksInput.addEventListener('input', function() {
+  const tasksList = document.querySelector('.tasks__list');
+  const button = document.querySelector('button');
+  button.addEventListener('click', (buttonEvent) => {
     if (this.value != '') {
-   let textHTML = `<div class="task">
-                      <div class="task__title">
-                        ${this.value}
-                      </div>
-                      <a href="#" class="task__remove">&times;</a>
-                  </div>`;
-    const tooltipAll1 = document.querySelector('.tasks__list');
-    tooltipAll1.insertAdjacentHTML('afterBegin', textHTML);
- }
-   this.value = '';
- }
-
- }, false);
+      console.log(this.value);
+      let textHTML = `<div class="task">
+                         <div class="task__title">
+                           ${this.value}
+                         </div>
+                         <a href="#" class="task__remove">&times;</a>
+                     </div>`;
+     tasksList.insertAdjacentHTML('afterBegin', textHTML);
+     this.value = '';
+    }
+  });
+  document.addEventListener('keydown', (eventKeydown) => {
+    if (eventKeydown.code == 'Enter') {
+      if (this.value != '') {
+        let textHTML = `<div class="task">
+                        <div class="task__title">
+                          ${this.value}
+                        </div>
+                        <a href="#" class="task__remove">&times;</a>
+                    </div>`;
+        tasksList.insertAdjacentHTML('afterBegin', textHTML);
+      }
+       this.value = '';
+    }
+  }, false);
 }, false);
