ca9413cd0eb9ff7a7fe5f960b986dfc98b6e2faf
colins1 <88490207+colins1@users.noreply.github.com>
2022-05-01 01:15:51 +0300

    add

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -3,12 +3,12 @@ document.addEventListener('click', function(click) {
   click.preventDefault();
 
   if (click.target.className == 'has-tooltip') {
-    let tooltipSearch = click.target.querySelector('.tooltip');
+    let tooltipSearch = click.target.parentNode.querySelector('.tooltip');
     if (tooltipSearch == null || tooltipSearch.className != 'tooltip tooltip_active') {
     allTooltip = document.querySelectorAll('.tooltip');
     let textHTML = `<div class="tooltip" data-position="bottom" style="left: ${click.target.offsetLeft}px;">${click.target.title}</div>`;
-    click.target.insertAdjacentHTML('beforeEnd', textHTML);
-    click.target.querySelector('.tooltip').classList.add("tooltip_active");
+    click.target.insertAdjacentHTML('afterEnd', textHTML);
+    click.target.nextSibling.classList.add("tooltip_active");
     if (allTooltip.length >= 1) {
       allTooltip.forEach(function(tooltipRemove) {
       if (tooltipRemove.parentNode.title != click.srcElement.title) {
@@ -17,6 +17,12 @@ document.addEventListener('click', function(click) {
       });
     }
   } else {
+
+    if (tooltipSearch.previousSibling.innerText != click.target.innerText) {
+      let textHTML2 = `<div class="tooltip" data-position="bottom" style="left: ${click.target.offsetLeft}px;">${click.target.title}</div>`;
+      click.target.insertAdjacentHTML('afterEnd', textHTML2);
+      click.target.nextSibling.classList.add("tooltip_active");
+    }
     tooltipSearch.remove();
   }
     }
