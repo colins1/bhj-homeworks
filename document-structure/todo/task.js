b81fc5636207bb925c7ca7716f242a9b35788a34
colins1 <88490207+colins1@users.noreply.github.com>
2022-05-01 02:26:42 +0300

    add

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -5,35 +5,39 @@ document.addEventListener('click', (eventClick) => {
     }
 }, false);
 
-const tasksInput = document.querySelector('.tasks__input');
-tasksInput.addEventListener('input', function() {
-  const tasksList = document.querySelector('.tasks__list');
-  const button = document.querySelector('button');
-  button.addEventListener('click', (buttonEvent) => {
-    if (this.value != '') {
-      console.log(this.value);
+const tasksList = document.querySelector('.tasks__list');
+
+
+const button = document.querySelector('button');
+button.addEventListener('click', (buttonEvent) => {
+  let input = document.querySelector('input');
+  if (input.value != '') {
+    let textHTML = `<div class="task">
+                       <div class="task__title">
+                         ${input.value}
+                       </div>
+                       <a href="#" class="task__remove">&times;</a>
+                   </div>`;
+   tasksList.insertAdjacentHTML('afterBegin', textHTML);
+
+   input.value = '';
+  }
+
+}, false);
+
+document.addEventListener('keydown', (eventKeydown) => {
+  let input = document.querySelector('input');
+  if (eventKeydown.code == 'Enter') {
+    if (input.value != '') {
       let textHTML = `<div class="task">
-                         <div class="task__title">
-                           ${this.value}
-                         </div>
-                         <a href="#" class="task__remove">&times;</a>
-                     </div>`;
-     tasksList.insertAdjacentHTML('afterBegin', textHTML);
-     this.value = '';
+                      <div class="task__title">
+                        ${input.value}
+                      </div>
+                      <a href="#" class="task__remove">&times;</a>
+                  </div>`;
+      tasksList.insertAdjacentHTML('afterBegin', textHTML);
     }
-  });
-  document.addEventListener('keydown', (eventKeydown) => {
-    if (eventKeydown.code == 'Enter') {
-      if (this.value != '') {
-        let textHTML = `<div class="task">
-                        <div class="task__title">
-                          ${this.value}
-                        </div>
-                        <a href="#" class="task__remove">&times;</a>
-                    </div>`;
-        tasksList.insertAdjacentHTML('afterBegin', textHTML);
-      }
-       this.value = '';
-    }
-  }, false);
+    input.value = '';
+  }
+
 }, false);
