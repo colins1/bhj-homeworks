4e3222e318145f378bf0396adccaadc619943188
colins1 <88490207+colins1@users.noreply.github.com>
2022-05-05 12:11:40 +0300

    add

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -5,18 +5,22 @@ xhr.addEventListener('readystatechange', () => {
     let jsonCode = Object.entries(JSON.parse(xhr.response))[1][1];
     document.querySelector('.poll__title').textContent = jsonCode.title;
     jsonCode.answers.forEach(function(textButton) {
-    let textHTML = `<button class="poll__answer">
-                      ${textButton}
-                    </button>`;
-    let pollButtons = document.querySelector('#poll__answers');
-    pollButtons.insertAdjacentHTML('afterBegin', textHTML);
+    let parent = document.querySelector('#poll__answers');
+
+    let buttons = document.createElement('button');
+    buttons.textContent = textButton
+    buttons.className = 'poll__answer'
+
+    parent.appendChild(buttons);
     });
+
+
 }
 })
 const btn = document.querySelector('#poll__answers');
   btn.addEventListener('click', (event) => {
     if (event.target.localName == 'button') {
-      alert(`«Спасибо, ваш голос засчитан!» вы выбрали ${event.target.innerText}`)
+      alert(`«Спасибо, ваш голос засчитан!»`)
     }
   }, false);
 xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/poll.php')
