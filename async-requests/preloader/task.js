76060cf60de753178a4d30b0ff0124d918daadfd
colins1 <88490207+colins1@users.noreply.github.com>
2022-05-05 14:29:27 +0300

    add

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -6,22 +6,29 @@ xhr.addEventListener('readystatechange', () => {
     const img = document.querySelector('img');
     img.classList.remove('loader_active');
     Object.entries(jsonCode.response.Valute).forEach(function(valute) {
-      console.log(valute)
-    let textHTML = `<div class="item">
-                    <div class="item__code">
-                    ${valute[1].CharCode}
-                    </div>
-                    <div class="item__value">
-                    ${valute[1].Value}
-                    </div>
-                    <div class="item__currency">
-                    руб.
-                    </div></div>`;
-    let blockItems = document.querySelector('#items');
-    blockItems.insertAdjacentHTML('afterBegin', textHTML);
+
+    let parent = document.querySelector('#items');
+    let divsItem = document.createElement('div');
+    divsItem.className = 'item'
+    let divItemCode = document.createElement('div');
+    divItemCode.className = 'item__code'
+    divItemCode.textContent = valute[1].CharCode
+    let divItemValue = document.createElement('div');
+    divItemValue.className = 'item__value'
+    divItemValue.textContent = valute[1].Value
+    let divItemCurrency = document.createElement('div')
+    divItemCurrency.className = 'item__currency'
+    divItemCurrency.textContent = 'руб.'
+
+    parent.appendChild(divsItem);
+    divsItem.appendChild(divItemCode);
+    divsItem.appendChild(divItemValue);
+    divsItem.appendChild(divItemCurrency);
   });
 }
 })
+
+
 
 xhr.open('POST', 'https://netology-slow-rest.herokuapp.com')
 
