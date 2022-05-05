0dfdb0ac948a30574189fe9cb1e644f82385e23e
colins1 <88490207+colins1@users.noreply.github.com>
2022-05-05 11:34:52 +0300

    addd

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -13,21 +13,6 @@ function checkedInterestCheck (input) {
         chekbox.checked = false;
       });
     }
-  } else {
-    const parentChekbox = ul.querySelectorAll('.interest__check');
-    if (input.checked == true) {
-      let parentUl = ul.parentElement;
-      parentUl.querySelector('.interest__check').checked = true;
-    } else {
-      let tryeChecked = Array.from(parentChekbox).some(elem => elem.checked == true);
-      if (tryeChecked) {
-        let parentUl = ul.parentElement;
-        parentUl.querySelector('.interest__check').checked = true;
-      } else {
-        let parentUl = ul.parentElement;
-        parentUl.querySelector('.interest__check').checked = false;
-      }
-    }
   }
 }
 
