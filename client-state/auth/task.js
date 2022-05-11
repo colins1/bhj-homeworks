360b412238c0b86614fe0441f7c66a6cee32c49d
colins1 <88490207+colins1@users.noreply.github.com>
2022-05-11 23:24:23 +0300

    add

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,31 +1,30 @@
 const formAuth = document.getElementById("signin__form");
-
 const xhr = new XMLHttpRequest();
 
 if (localStorage.user_id) {
-   user();
+ user();
 } else {
-   formAuth.addEventListener("submit", (e) => {
-      e.preventDefault();
-      const formData = new FormData(formAuth);
-      xhr.open("POST", formAuth.action);
-      xhr.send(formData);
-      xhr.responseType = "json";
-      xhr.addEventListener("load", () => {
-         if (xhr.response.user_id) {
-            localStorage.setItem(`user_id`, xhr.response.user_id);
-            user();
-         }
-         else {
-            alert("«Неверный логин/пароль»");
-            formAuth.reset();
-         }
-      })
-   })
+ formAuth.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(formAuth);
+    xhr.open("POST", formAuth.action);
+    xhr.send(formData);
+    xhr.responseType = "json";
+    xhr.addEventListener("load", () => {
+       if (xhr.response.user_id) {
+          localStorage.setItem(`user_id`, xhr.response.user_id);
+          user();
+       }
+       else {
+          alert("«Неверный логин/пароль»");
+          formAuth.reset();
+       }
+    })
+ })
 }
 
 function user() {
-   document.getElementById("signin").classList.remove("signin_active");
-   document.getElementsByClassName("welcome")[0].classList.add("welcome_active");
-   document.getElementById("user_id").textContent = localStorage.getItem(`user_id`);
+ document.getElementById("signin").classList.remove("signin_active");
+ document.getElementsByClassName("welcome")[0].classList.add("welcome_active");
+ document.getElementById("user_id").textContent = localStorage.getItem(`user_id`);
 }
